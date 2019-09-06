9bd0dfaab8d31be75a1fc7c9726c469e60789f8b
Plimon Howard V <plimon.howard@gmail.com>
2019-09-06 06:37:32 +0000

    Automatically backed up by Learn

--- lib/array_crud.rb
+++ lib/array_crud.rb
@@ -42,5 +42,6 @@ def retrieve_last_element_from_array(array)
 end
 
 def update_element_from_index(array, index_number, element)
-  array = array[index_number].add(element)
+  array[index_number].add(element)
+  return array
 end
